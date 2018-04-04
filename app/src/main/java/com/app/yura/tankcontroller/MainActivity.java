fd569a6089509abb7b5cb0a9670dd504822d56fd
yoopibit <yavorskyi.yurii@gmail.com>
2018-04-05 02:33:56 +0300

    add AlertDialog on exit from MainActivity

--- app/src/main/java/com/app/yura/tankcontroller/MainActivity.java
+++ app/src/main/java/com/app/yura/tankcontroller/MainActivity.java
@@ -1,12 +1,11 @@
 package com.app.yura.tankcontroller;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.graphics.Canvas;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.AttributeSet;
-import android.view.MotionEvent;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -158,6 +157,35 @@ public class MainActivity extends AppCompatActivity {
     {
         web.loadUrl("http://192.168.8.1:8080");
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            exitByBackKey();
+            
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    protected void exitByBackKey() {
+
+        AlertDialog alertbox = new AlertDialog.Builder(this)
+                .setMessage("Do you want to close connection?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                    }
+                })
+                .show();
+
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
